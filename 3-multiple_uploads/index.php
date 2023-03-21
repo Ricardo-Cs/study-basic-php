ee2982df55edea212724ef361641d2836dd2246c
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-03-21 16:14:39 -0500

    Conteúdo upload de múltiplos arquivos

---
+++ 3-multiple_uploads/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload de Arquivos</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['enviar-form'])) {
+        $formatosPermitidos = array("png", "jpeg", "jpg", "gif", "txt");
+        $quantidadeArquivos = count($_FILES['arquivo']['name']);
+        $contador = 0;
+
+        while ($contador < $quantidadeArquivos) {
+            $extensao = pathinfo($_FILES["arquivo"]["name"][$contador], PATHINFO_EXTENSION); // O terceiro parâmetro é o indice do arquivo
+            if (in_array($extensao, $formatosPermitidos)) { // Verifica se o parâmetro 1 está no parâmetro 2 (array)
+                $pasta = "files/";
+                $temporario = $_FILES['arquivo']['tmp_name'][$contador]; //Pega o caminho temporário do arquivo enviado (primeiro parametro)
+                $newName = uniqid() . ".$extensao"; //uniqid() gera um id único.
+
+                if (move_uploaded_file($temporario, $pasta . $newName)) //Move o arquivo enviado para o segundo parâmetro, nesse caso a pasta files, junto com o id e sua extensão
+                    echo "Upload feito com sucesso para $pasta.$newName<br>";
+                else
+                    echo "Erro ao enviar o arquivo $temporario<br>";
+            } else
+                echo "Arquivo com extensão .$extensao não são permitidos!<br>";
+            $contador++;
+        }
+    }
+    ?>
+
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data"> <!-- enctype="multipart/form-data" é obrigatório ao enviar arquivos -->
+        <input type="file" name="arquivo[]" multiple>
+        <br><br>
+        <input type="submit" value="Enviar" name="enviar-form">
+    </form>
+</body>
+
+</html>
