129cd51584c38e21882c4f228cfc0781a8c98c50
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-03-23 15:42:38 -0500

    CRUD terminado

--- 6-crud-PHP-procedural/index.php
+++ 6-crud-PHP-procedural/index.php
@@ -23,17 +23,40 @@ include_once './includes/mensagem.php';
         <?php
         $sql = "SELECT * FROM clientes";
         $resultado = mysqli_query($connect, $sql);
-        while ($dados = mysqli_fetch_array($resultado)) {
+
+        if (mysqli_num_rows($resultado) > 0) {
+          while ($dados = mysqli_fetch_array($resultado)) {
         ?>
-          <tr>
-            <td><?php echo $dados['nome']; ?></td>
-            <td><?php echo $dados['sobrenome']; ?></td>
-            <td><?php echo $dados['email']; ?></td>
-            <td><?php echo $dados['idade']; ?></td>
-            <td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
-            <td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
-          </tr>
-        <?php } ?>
+            <tr>
+              <td><?php echo $dados['nome']; ?></td>
+              <td><?php echo $dados['sobrenome']; ?></td>
+              <td><?php echo $dados['email']; ?></td>
+              <td><?php echo $dados['idade']; ?></td>
+              <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+              <td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
+
+              <!-- Modal Structure -->
+              <div id="modal<?php echo $dados['id']; ?>" class="modal">
+                <div class="modal-content">
+                  <h4>Opa!</h4>
+                  <p>Deseja mesmo deletar esse cliente?</p>
+                </div>
+                <div class="modal-footer">
+                  <form action="php_action/delete.php" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                    <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
+                    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
+
+                  </form>
+                </div>
+              </div>
+            </tr>
+        <?php
+          }
+        }
+        ?>
+
+
 
       </tbody>
     </table>
